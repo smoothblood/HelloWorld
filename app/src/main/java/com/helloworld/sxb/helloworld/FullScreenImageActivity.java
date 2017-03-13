5b0a8d2ebe459d186a567fd78e2513bdf2618712
SBE-PC\hp850g2-PC <b.sa3d17@gmail.com>
2017-03-13 17:05:44 +0000

    Initial commit (13-03-2017)

---
+++ app/src/main/java/com/helloworld/sxb/helloworld/FullScreenImageActivity.java
@@ -0,0 +1,18 @@
+package com.helloworld.sxb.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FullScreenImageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_full_screen_image);
+
+
+
+    }
+
+
+}
