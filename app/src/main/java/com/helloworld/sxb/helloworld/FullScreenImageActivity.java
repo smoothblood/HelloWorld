6e58243f8169fbb0dd9dc57d07630414e5e703ce
SBE-PC\hp850g2-PC <b.sa3d17@gmail.com>
2017-03-14 12:05:08 +0000

    pass data throuh intent

--- app/src/main/java/com/helloworld/sxb/helloworld/FullScreenImageActivity.java
+++ app/src/main/java/com/helloworld/sxb/helloworld/FullScreenImageActivity.java
@@ -1,7 +1,13 @@
 package com.helloworld.sxb.helloworld;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 public class FullScreenImageActivity extends AppCompatActivity {
 
@@ -10,7 +16,19 @@ public class FullScreenImageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_full_screen_image);
 
+        ImageView fullScreenImageView = (ImageView) findViewById(R.id.fullScreenimageView);
+        TextView txtView = (TextView) findViewById(R.id.textView3);
 
+
+        Intent callingActivityIntent = getIntent();
+        if(callingActivityIntent != null){
+            String msg = callingActivityIntent.getStringExtra("msg");
+            txtView.setText(msg);
+//            Uri imageUri = callingActivityIntent.getData();
+//            if(imageUri != null && fullScreenImageView != null){
+//                Glide.with(this).load(imageUri).into(fullScreenImageView);
+//            }
+        }
 
     }
 
--- app/src/main/java/com/helloworld/sxb/helloworld/HelloWorldActivity.java
+++ app/src/main/java/com/helloworld/sxb/helloworld/HelloWorldActivity.java
@@ -1,7 +1,12 @@
 package com.helloworld.sxb.helloworld;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class HelloWorldActivity extends AppCompatActivity {
 
@@ -9,6 +14,29 @@ public class HelloWorldActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello_world);
+
+
+        final ImageView img = (ImageView)findViewById(R.id.imageView);
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Show messaga
+                Context context = getApplicationContext();
+                CharSequence textOnClick = "That me ;) \ud83d\ude01";
+                int duration = Toast.LENGTH_SHORT;
+
+                Toast toast = Toast.makeText(context, textOnClick, duration);
+                toast.show();
+
+                // Enlarge image
+                Intent fullScreenIntent = new Intent(getBaseContext(), FullScreenImageActivity.class);
+                fullScreenIntent.putExtra("msg", "From HelloWorld Activity");
+                startActivity(fullScreenIntent);
+            }
+        });
+
     }
+
+
 
 }
